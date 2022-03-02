10ba25570dc96f6c423570e91d80ac5de880307c
yamashita rintaro <Yamarin0717@gmail.com>
2022-03-02 13:10:05 +0900

    Add: カテゴリー機能を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_044703) do
+ActiveRecord::Schema.define(version: 2022_03_01_130829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
