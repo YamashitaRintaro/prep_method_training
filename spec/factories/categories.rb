781b773d119f49d7daefad481daa77a79d3a6264
yamashita rintaro <Yamarin0717@gmail.com>
2022-04-12 20:24:10 +0900

    Add: userモデルスペック

---
+++ spec/factories/categories.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :category do
+    name { '新卒' }
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーション確認" do
+    let(:user) { create(:user) }
+    
+    it 'email, category_id, password, password_confirmationがあれば有効であること' do
+      expect(user).to be_valid
+    end
+    
+    it 'emailが無ければ無効であること' do
+      user.email = nil
+      expect(user).to be_invalid
+    end
+    
+    it 'emailが重複している場合、無効であること' do
+      other_user = build(:user, email: user.email)
+      expect(other_user).to be_invalid
+    end
+    
+    it 'emailが重複していない場合、有効であること' do
+      other_user = build(:user)
+      expect(other_user).to be_valid
+    end
+  
+    it 'category_idが無ければ無効であること' do
+      user.category_id = nil
+      expect(user).to be_invalid
+    end
+    
+    it 'passwordが無ければ無効であること' do
+      user = build(:user, password: nil)
+      expect(user).to be_invalid
+    end
+    
+    it 'password_confirmationが無ければ無効であること' do
+      user = build(:user, password_confirmation: nil)
+      expect(user).to be_invalid
+    end
+  end
 end
