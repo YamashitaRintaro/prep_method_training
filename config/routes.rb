e43e9d1d47c0e72dbf95afa91c5df41a77d3ca2e
yamashita rintaro <Yamarin0717@gmail.com>
2022-04-12 15:33:57 +0900

    Add: プロフィール機能を追加 & ja.ymlに追記

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
   resources :users, only: %i[new create]
+  resource :profile, only: %i[show edit update]
   resources :categories
   resources :questions
   resources :trainings, only: %i[show new create destroy] do
