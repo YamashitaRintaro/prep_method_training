aea39fc94785ceccb49d1c46925721b61723ece2
yamashita rintaro <Yamarin0717@gmail.com>
2022-04-15 15:22:16 +0900

    Add: 質問を選択していない場合、「次へ」ボタンをdisabledに

--- app/javascript/packs/training.js
+++ app/javascript/packs/training.js
@@ -1,9 +1,12 @@
 let opts = document.querySelectorAll('.radio');
+const next = document.getElementById('next');
 for (let i = 0; i < opts.length; i++) {
   let opt = opts[i];
   opt.addEventListener('click', function(){
   console.log(this.id + '-title');
   hiddenRadio = document.getElementById(this.id + '-title');
   hiddenRadio.click();
+  next.disabled = false;
 });
 }
+
