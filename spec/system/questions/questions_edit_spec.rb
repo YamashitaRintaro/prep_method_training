993b65ad6150c0348df36a2783c975efd37a2eff
yamashita rintaro <Yamarin0717@gmail.com>
2022-04-16 19:08:46 +0900

    Add: trainings_newシステムスペック

--- spec/system/questions/questions_edit_spec.rb
+++ spec/system/questions/questions_edit_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Question#edit", type: :system do
-  fdescribe '質問編集' do
+  describe '質問編集' do
       let(:user) { create(:user) }
       let(:admin) { create(:user, :admin) }
       let(:category) { create(:category) }
