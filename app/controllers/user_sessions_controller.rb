476b92131e549b7e187c2f597c527178656262c7
yamashita rintaro <Yamarin0717@gmail.com>
2022-04-20 20:00:27 +0900

    Add: ゲストログイン機能を追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,5 +1,5 @@
 class UserSessionsController < ApplicationController
-  skip_before_action :require_login, only: %i[new create]
+  skip_before_action :require_login, only: %i[new create guest_login]
   def new; end
 
   def create
@@ -13,7 +13,17 @@ class UserSessionsController < ApplicationController
   end
 
   def destroy
+    current_user.destroy! if current_user.guest?
     logout
     redirect_to root_path, success: 'ログアウトしました'
+  end
+
+  def guest_login
+    redirect_to root_path, alert: 'すでにログインしています' if current_user # ログインしてる場合はユーザーを作成しない
+
+    random_value = SecureRandom.hex
+    user = User.create!(email: "guest_#{random_value}@example.com", role: :guest, category_id: 1, password: random_value, password_confirmation: random_value)
+    auto_login(user)
+    redirect_to new_training_path
   end
 end
