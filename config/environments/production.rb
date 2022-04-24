ab10d71b36a77daca9330957b22f694171f64e77
yamashita rintaro <Yamarin0717@gmail.com>
2022-04-24 13:13:16 +0900

    Remove: STARTTLSで暗号化される設定になっているため、plain認証に戻す

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
     domain: 'gmail.com',
     user_name: ENV['MAIL_ADDRESS'],
     password: ENV['MAIL_PASSWORD'],
-    authentication: 'login',
+    authentication: 'plain',
     enable_starttls_auto: true
   }
 
