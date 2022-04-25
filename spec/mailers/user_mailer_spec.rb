8c036e0886b532420552ebdc946c4ecfbbcf1fbf
yamashita rintaro <Yamarin0717@gmail.com>
2022-04-25 13:55:23 +0900

    Add: パスワードリセットのメール送信テストを追加

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,17 +1,20 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  describe 'reset_password_email' do
-    let(:mail) { UserMailer.reset_password_email }
+  fdescribe "reset_password_email" do
+    let(:user) { create(:user) }
+    let(:mail) { UserMailer.reset_password_email(user) }
+    # Sorceryのgenerate_reset_password_token!メソッドでトークンを発行
+    before { user.generate_reset_password_token! }
 
-    it 'renders the headers' do
-      expect(mail.subject).to eq('Reset password email')
-      expect(mail.to).to eq(['to@example.org'])
-      expect(mail.from).to eq(['from@example.com'])
-    end
-
-    it 'renders the body' do
-      expect(mail.body.encoded).to match('Hi')
+    it "ヘッダー情報・ボディ情報が正しいこと" do
+      # メールを送信して、メールの件数が1件増えることを確認
+      expect do
+        mail.deliver_now
+      end.to change { ActionMailer::Base.deliveries.size }.by(1)
+      expect(mail.subject).to eq("パスワードリセット")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["from@example.com"])
     end
   end
 end
