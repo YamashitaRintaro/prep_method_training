dbdf5525a688fc3cc1927047e1f4c874f91816f2
yamashita rintaro <Yamarin0717@gmail.com>
2022-05-03 20:54:21 +0900

    Add: 外部認証の共通処理

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  has_many :authentications, dependent: :destroy
+  accepts_nested_attributes_for :authentications
   has_many :trainings, dependent: :destroy
   belongs_to :category
 
