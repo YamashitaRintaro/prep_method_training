0f5607be46fbc2fa7a7506b071473f6b0bc1d924
yamashita rintaro <Yamarin0717@gmail.com>
2022-05-08 23:30:38 +0900

    Add: カテゴリー未登録ユーザーのためのカテゴリー選択機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   post 'guest_login', to: 'user_sessions#guest_login'
   resources :users, only: %i[new create]
   resource :profile, only: %i[show edit update]
+  resource :user_category, only: %i[edit update]
   resources :categories
   resources :questions
   resources :trainings, only: %i[show new create destroy] do
