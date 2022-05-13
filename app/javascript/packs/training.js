4eb79ad03313b575dc602ade7bb8cdf79ba11e59
yamashita rintaro <Yamarin0717@gmail.com>
2022-05-13 23:05:41 +0900

    Add: ラジオボタンのラベルにクリック判定をつける

--- app/javascript/packs/training.js
+++ app/javascript/packs/training.js
@@ -3,8 +3,7 @@ const next = document.getElementById('next');
 for (let i = 0; i < opts.length; i++) {
   let opt = opts[i];
   opt.addEventListener('click', function(){
-  console.log(this.id + '-title');
-  hiddenRadio = document.getElementById(this.id + '-title');
+  hiddenRadio = document.getElementById(this.id + '_title');
   hiddenRadio.click();
   next.disabled = false;
 });
