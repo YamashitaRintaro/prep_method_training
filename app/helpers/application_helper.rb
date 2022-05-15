12de34dd40f1dbc7557bfa7024e8d45d3cae8489
yamashita rintaro <Yamarin0717@gmail.com>
2022-05-16 00:03:41 +0900

    Add: 各ページに固有のページタイトルを設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
+  def page_title(page_title = '', admin = false)
+    base_title = if admin
+                   'Prep(管理画面)'
+                 else
+                   'Prep'
+                 end
+
+    page_title.empty? ? base_title : page_title + ' | ' + base_title
+  end
+
   def default_meta_tags
     {
       title: 'Prep',
