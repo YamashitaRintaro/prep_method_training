f8e695bdbb4b408731d81220651f0fb62905be53
yamashita rintaro <Yamarin0717@gmail.com>
2022-05-20 16:14:16 +0900

    Add: memoモデルのバリデーションと関連を追加

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,3 +1,4 @@
 class Memo < ApplicationRecord
   belongs_to :training
+  validates :body, presence: true, length: { maximum: 65_535 }
 end
--- app/models/training.rb
+++ app/models/training.rb
@@ -2,6 +2,7 @@ class Training < ApplicationRecord
   belongs_to :user
   belongs_to :question
   has_many :voices, dependent: :destroy
+  has_one :memo, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 255 }
   scope :published, -> { question.title }
